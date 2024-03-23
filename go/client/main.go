726c6b8229d27b18d9100bb476a92d7472baf77c
sane <374023109.com>
2024-03-23 14:55:28 +0800

    go 原生 rpc

---
+++ go/client/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/rpc"
+
+func main() {
+	conn, err := rpc.Dial("tcp", "127.0.0.1:9901")
+	if err != nil {
+		panic(err)
+	}
+
+	var reply string
+	err = conn.Call("HelloService.Hello", "hello", &reply)
+	if err != nil {
+		panic(err)
+	}
+
+	println(reply)
+}
---
+++ go/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net"
+	"net/rpc"
+)
+
+func main() {
+
+	_ = rpc.RegisterName("HelloService", new(HelloService))
+
+	listener, err := net.Listen("tcp", "127.0.0.1:9901")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go rpc.ServeConn(conn)
+	}
+}
